a1b3820964952d078b1f7bcf7d963c95d23db603
Lucas <lucasgoncalves08@live.com>
2023-12-01 19:13:01 -0300

    working on the auth component's styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { AuthComponent } from './components/auth/auth.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'entertainment';
+
 }
