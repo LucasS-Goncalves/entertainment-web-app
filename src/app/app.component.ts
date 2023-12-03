c9a41a1860f0ea06a9cb6d0b68c005a5a9821d8d
Lucas <lucasgoncalves08@live.com>
2023-12-03 15:26:15 -0300

    working on search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet, Router } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AuthComponent, HomeComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, AuthComponent, HomeComponent, NavbarComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
