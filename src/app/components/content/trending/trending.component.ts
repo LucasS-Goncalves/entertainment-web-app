3a46c2554f934bc1fa274c0c929c88c6c87037e8
Lucas <lucasgoncalves08@live.com>
2023-12-27 18:58:33 -0300

    working on card in the trending component

--- src/app/components/content/trending/trending.component.ts
+++ src/app/components/content/trending/trending.component.ts
@@ -12,27 +12,68 @@ import { CardComponent } from '../card/card.component';
 export class TrendingComponent {
 
   photos = [
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
-    "../../../../assets/thumbnails/112/regular/medium.jpg",
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: false,
+      type: 'Movie',
+      year: '2020',
+      contentRating: '18+',
+      title: 'Beyond Earth'
+    },
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: true,
+      type: 'TV Series',
+      year: '2010',
+      contentRating: 'PG',
+      title: 'Bottom Gear'
+    },
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: false,
+      type: 'TV Series',
+      year: '2022',
+      contentRating: 'PG',
+      title: 'Beyond Earth'
+    },
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: false,
+      type: 'Movie',
+      year: '2012',
+      contentRating: 'PG',
+      title: 'Beyond Earth'
+    },
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: false,
+      type: 'Movie',
+      year: '2015',
+      contentRating: 'PG',
+      title: 'Beyond Earth'
+    },
+    {
+      url: "../../../../assets/thumbnails/112/regular/medium.jpg",
+      isBookmarked: false,
+      type: 'Movie',
+      year: '2021',
+      contentRating: 'PG',
+      title: 'Beyond Earth'
+    },
   ];
 
   @ViewChild('imageContainer') imageContainer!: ElementRef<HTMLDivElement>;
 
   scrollToLeft(){
-    this.imageContainer.nativeElement.scrollLeft = this.imageContainer.nativeElement.scrollLeft - 250;
+    this.imageContainer.nativeElement.scrollLeft = this.imageContainer.nativeElement.scrollLeft - 400;
   }
 
   scrollToRight(){
-    this.imageContainer.nativeElement.scrollLeft = this.imageContainer.nativeElement.scrollLeft + 233;
+    this.imageContainer.nativeElement.scrollLeft = this.imageContainer.nativeElement.scrollLeft + 400;
   }
 
+  isBookmarked(index: number){
+    this.photos[index].isBookmarked = !this.photos[index].isBookmarked;
+  }
 
 }
